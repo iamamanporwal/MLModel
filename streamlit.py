82de4a81273243a2bedbf86a5aaef986ef5dd9de
iamamanporwal <74871831+iamamanporwal@users.noreply.github.com>
2023-02-07 12:19:55 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import pickle
+import sklearn
+import pandas as pd
+import numpy as np
+from PIL import Image 
+
+model = pickle.load(open('model.sav', 'rb'))
+
+st.title('prediction model')
+st.sidebar.header('Data')
+image = Image.open(image.jpg)
+st.image(image, '')
